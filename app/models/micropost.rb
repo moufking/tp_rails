7d3c12db15664db695d44dc554d3257456507dbd
Moufid <moufking337@gmail.com>
2018-09-18 12:06:38 +0100

    Chapitre2

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,4 @@
 class Micropost < ApplicationRecord
-    validates :content, length: { maximum: 12 }
+    belongs_to :user
+    validates :content, length: { maximum: 12 }, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :name, length: { maximum: 12 },presence: true
+
+    validates :email, length: { maximum: 12 },presence: true
+
+
 end
