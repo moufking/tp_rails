d5e18994ac9fe521bf23251c8d3dd60934093a83
Moufid <moufking337@gmail.com>
2018-09-19 02:04:36 +0100

    chapitre 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   resources :microposts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users
-  root 'users#index'
+  #root 'users#index'
+  get  'static_pages/about'
+  #root 'application#hello'
+  get 'static_pages/contact'
+
 end
